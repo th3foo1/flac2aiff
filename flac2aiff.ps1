0db43db0a502f5ca40d7d0dde9be13839a9fce5f
th3foo1 <68256249+th3foo1@users.noreply.github.com>
2024-04-12 20:45:36 +0800

    Add files via upload

---
+++ flac2aiff.ps1
@@ -0,0 +1,18 @@
+# Prompt the user for the input and output directories
+$inputDir = Read-Host -Prompt "Enter the input directory path"
+$outputDir = Read-Host -Prompt "Enter the output directory path"
+
+if (!(Test-Path -Path $outputDir)) {
+    New-Item -Path $outputDir -ItemType Directory | Out-Null
+}
+
+# Get the flac files in the input directory
+Get-ChildItem -Path $inputDir -Filter *.flac | ForEach-Object {
+    $inputFile = Join-Path -Path $inputDir -ChildPath $_.name
+    $inputFile
+    # Make the output file the same name as flac but with .aiff file name
+    $outputFile = Join-Path -Path $outputDir -ChildPath ($_.BaseName + ".aiff")
+    
+    # Run the ffmpeg command
+    ffmpeg -i $inputFile -write_id3v2 1 -c:v copy -acodec pcm_s16le -ar 44100 -ac 2 $outputFile
+}
